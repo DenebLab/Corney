71d2d079da89d58e1cd9ec3b6c3a5bc8d06604d0
Piotr Kudrel <piotr.kudrel@deneblab.com>
2025-08-05 15:49:27 +0800

    SAVEPOINT

--- src/build/Build.cs
+++ src/build/Build.cs
@@ -33,6 +33,7 @@ class Build : NukeBuild
 
     AbcVersion AbcVersion => AbcVersionFactory.CreateOneBuilder()
         .SetDateTime(BuildDate)
+        .SetRepositoryPath(RootDirectory)
         .Build(); // Creates new instance
 
     Target Information => _ => _
